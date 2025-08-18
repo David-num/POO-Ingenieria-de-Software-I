c29ca5a49cc1931e1e1b6bd3d1a9b199a0094249
David-num <124700120+David-num@users.noreply.github.com>
2025-08-17 18:17:36 -0600

    Retorno antes de abstracción
    Se realizo  la abstracción antes de lo planeado por lo que se regreso a un estado donde no se a implementado la abstracción y se corrigió la contraseña que fallaba.

--- poo/src/app/api/route.ts
+++ poo/src/app/api/route.ts
@@ -4,69 +4,48 @@ import postgres from "postgres";
 export async function POST(request: NextRequest) {
 
   try {
+
     const data = await request.json();
     const { title, description, author } = data;
 
-    isValidTitle(title);
-    isValidDescription(description);
-    isValidAuthor(author);
+    if (typeof title !== "string") {
+      throw new Error("Invalid title format");
+    }
 
-    savePostData({ title, description, author });
+    if (!(title.length > 0 && title.length <= 80)) {
+      throw new Error("Invalid title length");
+    }
+
+    if (typeof description !== "string") {
+      throw new Error("Invalid description format");
+    }
+
+    if (!(description.length > 0 && description.length <= 500)) {
+      throw new Error("Invalid description length");
+    }
+
+    if (typeof author !== "string") {
+      throw new Error("Invalid author format");
+    }
+
+    if (!(author.length > 0 && author.length <= 40)) {
+      throw new Error("Invalid author length");
+    }
+
+    try {
+
+      const connectionString = "postgresql://postgres.edxdrjgaiwftvtpcyeii:"+
+      "laOSUDvlWF4l937H@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+
+      const sql = postgres(connectionString);
+
+      await sql`INSERT INTO public.post (title, description, author) VALUES (${title}, ${description}, ${author})`;
+    } catch (error) {
+      throw new Error("Failed to save post");
+    }
 
     return NextResponse.json(
-      { message: "Data is valid" }
+      { message: "Data is valid" },
     );
-
-  } catch (error) {
-    console.error("Error processing request:", error);
-    return NextResponse.json(
-      { error: "Error validating data" },
-      { status: 400 }
-    );
-  }
-}
-
-async function savePostData(data: { title: string; description: string; author: string }): Promise<void> {
-
-  try {
-    const connectionString = "postgresql://postgres.edxdrjgaiwftvtpcyeii:fWHLH7Pj7QPpDDzn@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
-    const sql = postgres(connectionString);
-
-    await sql`INSERT INTO public.posts (title, description, author) VALUES (${data.title}, ${data.description}, ${data.author})`;
-  } catch (error) {
-    throw new Error("Failed to save post");
-  }
-}
-
-function isValidTitle(title: string): void {
-
-  if (typeof title !== "string") {
-    throw new Error("Invalid title format");
-  }
-
-  if (!(title.length > 0 && title.length <= 80)) {
-    throw new Error("Invalid title length");
-  }
-}
-
-function isValidDescription(description: string): void {
-
-  if (typeof description !== "string") {
-    throw new Error("Invalid description format");
-  }
-
-  if (!(description.length > 0 && description.length <= 500)) {
-    throw new Error("Invalid description length");
-  }
-}
-
-function isValidAuthor(author: string): void {
-
-  if (typeof author !== "string") {
-    throw new Error("Invalid author format");
-  }
-
-  if (!(author.length > 0 && author.length <= 40)) {
-    throw new Error("Invalid author length");
-  }
+  } finally {}
 }
